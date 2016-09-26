d7cb0a0a940c89e61b8fec1bc602e6976f28b284
Temnoved <temnoved@yandex.ru>
2016-09-26 19:54:05 +0300

    Add files via upload
    DZ(22.09.2016)CalcSharp

---
+++ Calc.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZCalcSharp
+{
+    class Calc
+    {
+        protected String[] strNums = null;//массив чисел из выражения
+        protected char[] chSymbols = null;//массив символов из выражения
+        protected double dResult = 0;//результат выражения
+        protected String strEnterNum = "";//изночальнный ввод
+
+        //конструктор  параметром по умолчанию
+        public Calc(String strEntr = "")
+        {
+            this.strEnterNum = strEntr;
+
+            //создаём массив строк содержащий цифры из введенного примера
+            char [] chSep = { '*', '/', '+', '-' };
+            this.strNums = this.strEnterNum.Split(chSep, StringSplitOptions.RemoveEmptyEntries);
+
+            //создаём новый массив для знаков из премера(их всегда будет на один меньше чем цифр)
+            chSymbols = new char[this.strNums.Length - 1];
+
+            //складываем в массив знаки из премера
+            for(int i = 0; i < chSymbols.Length; i++)
+            {
+                int j = strEntr.IndexOfAny(chSep);
+                chSymbols[i] = strEntr[j];
+                strEntr = strEntr.Remove(j, 1);
+            }            
+        }
+
+        public double Result//для вывода результата
+        {
+            get { return this.dResult; }
+        }
+
+        public String Ishod//ддя вывода исходного выражения
+        {
+            get { return this.strEnterNum; }
+        }
+
+        public void Calculation()//подсчет результата
+        {
+            double dTempRes1 = 0, dTempRes2 = 0;//временные результаты промежуточных операций
+            int i = 0;//индекс массивов
+            
+            //если знаков больше чем один и первый имеет меньший приоритет чем второй то вычисление начинается со второй операции
+            if((i + 1) < this.chSymbols.Length && (this.chSymbols[i + 1] == '*' || this.chSymbols[i + 1] == '/') && (this.chSymbols[i] != '*' && this.chSymbols[i] != '/'))
+            {
+                if(this.chSymbols[i + 1] == '*')
+                {
+                    dTempRes2 = (double)((double.Parse(this.strNums[i + 1]) * (double.Parse(this.strNums[i + 2]))));
+                }
+                if (this.chSymbols[i + 1] == '/')
+                {
+                    dTempRes2 = (double)((double.Parse(this.strNums[i + 1]) / (double.Parse(this.strNums[i + 2]))));
+                }
+                if (this.chSymbols[i] == '+')
+                {
+                    dTempRes1 = (double)((double.Parse(this.strNums[i]) + dTempRes2));
+                }
+                if (this.chSymbols[i] == '-')
+                {
+                    dTempRes1 = (double)((double.Parse(this.strNums[i]) - dTempRes2));
+                }
+                this.dResult = dTempRes1;
+            }
+            //если приоритет операций не сдвинут - вы полняем последовательно справа на лево
+            else
+            {
+                //блок вариантов для первого знака
+                if(this.chSymbols[i] == '*')
+                {
+                    dTempRes1 = (double)((double.Parse(this.strNums[i]) * (double.Parse(this.strNums[i + 1]))));
+                }
+                if (this.chSymbols[i] == '/')
+                {
+                    dTempRes1 = (double)((double.Parse(this.strNums[i]) / (double.Parse(this.strNums[i + 1]))));
+                }
+                if (this.chSymbols[i] == '+')
+                {
+                    dTempRes1 = (double)((double.Parse(this.strNums[i]) + (double.Parse(this.strNums[i + 1]))));
+                }
+                if (this.chSymbols[i] == '-')
+                {
+                    dTempRes1 = (double)((double.Parse(this.strNums[i]) - (double.Parse(this.strNums[i + 1]))));
+                }
+
+                i++;//сдвигаемся в массивах чисел и знаков на один\
+                //блок вариантов для второго знака
+                if(i > this.chSymbols.Length - 1)
+                {
+                    this.dResult = dTempRes1;
+                }
+
+                if (this.chSymbols[i] == '*')
+                {
+                    dTempRes1 *= double.Parse(this.strNums[i + 1]);
+                }
+                if (this.chSymbols[i] == '/')
+                {
+                    dTempRes1 /= double.Parse(this.strNums[i + 1]);
+                }
+                if (this.chSymbols[i] == '+')
+                {
+                    dTempRes1 += double.Parse(this.strNums[i + 1]);
+                }
+                if (this.chSymbols[i] == '-')
+                {
+                    dTempRes1 -= double.Parse(this.strNums[i + 1]);
+                }
+                this.dResult = dTempRes1;
+            }
+            
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿//написать калькулятор, который должен принимать с командной строки при запуске(вспоминайте наше 1е занятие и параметр в методе Main ) математический
+//пример: 2 + 2 * 3, вычисляет результат и выводит на консоль в:
+//1) десятичном виде
+//2) Hex
+//3) двоичном виде.
+//Заранее известно, что математических операций в примере не может быть более двух, но може быть одно: 2 * 2. Калькулятор должен уметь вычислять: */+-. 
+//Если программа запускается без параметров, тогда должна вывестись справка на консоль, версия программы и имя разработчика.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZCalcSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 0)//в случае если программа запускается без параметров выводим справку
+                {
+                    Console.WriteLine("Program: CalcSharp v0.1_alpfa (2016)\nDevelop: Podoplelov Alexander");
+                    Console.WriteLine("Для использования программы после имени исполняемого файла\nвведите выражение (например:DZ(22.09.2016)CalcSharp.exe 1+2*3)");
+                    Console.WriteLine("ВНИМАНИЕ! Программа работает только с операциями *, /, + и -");
+                    Console.WriteLine("ВНИМАНИЕ! Программа работает только с целыми числами");
+                    Console.WriteLine("Результат будет выведен в форматах:\nDEC - десятичный; HEX - шестнадцатиричный; BIN - двоичный;");
+                    Console.WriteLine("\nДля продолжения нажмите Enter");
+                    Console.Read();
+                    return;
+                }
+
+                Calc cTest = new Calc(args[0]);//создаём объект класса с параметрами запуска
+                cTest.Calculation();//вычесляем результат
+
+                Console.WriteLine(cTest.Ishod + " = ");//показываем исходное выражение
+                Console.WriteLine("DEC: " + (int)cTest.Result);//показываем результат в десятичной форме
+
+
+
+                //два варианта перевода результата в шестнадцатиричную форму
+                //Console.WriteLine("HEX: " + (Convert.ToString((int)cTest.Result, 16)));
+                Console.WriteLine("HEX: " + ((int)cTest.Result).ToString("X"));
+
+                //cTest.toBin();
+                //перевод результата в двоичную форму
+                Console.WriteLine("BIN: " + Convert.ToString((int)cTest.Result, 2));
+
+
+                Console.Read();//пауза
+                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.Read();
+            }
+
+        }//конецMain
+    }//конец class Program
+}//конецnamespace DZ2016CalcSharp
